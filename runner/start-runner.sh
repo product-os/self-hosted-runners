23b3c0d75bda4f4d8743e1290fdfbc4453e6f789
Kyle Harding <kyle@balena.io>
2023-05-08 09:19:45 -0400

    Remove balena uuid label as runners are not being picked up
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- runner/start-runner.sh
+++ runner/start-runner.sh
@@ -41,6 +41,8 @@ function get_geo() {
 
 get_geo
 
+echo "${ipinfo}" | jq
+
 runner_tags=()
 runner_tags+=("family=$(cat </etc/lsb-release | grep DISTRIB_ID | awk -F'=' '{print $2}')")
 runner_tags+=("distro=$(cat </etc/lsb-release | grep DISTRIB_CODENAME | awk -F'=' '{print $2}' | sed 's/"//g')")
@@ -55,7 +57,6 @@ runner_tags+=("country=$(echo "${ipinfo}" | jq -r .country | tr ' ' '_' | tr '[:
 runner_tags+=("region=$(echo "${ipinfo}" | jq -r .region | tr ' ' '_' | tr '[:upper:]' '[:lower:]')")
 runner_tags+=("city=$(echo "${ipinfo}" | jq -r .city | tr ' ' '_' | tr '[:upper:]' '[:lower:]')")
 runner_tags+=("platform=${ACTIONS_RUNNER_PLATFORM}")
-runner_tags+=("balena_device_uuid=${BALENA_DEVICE_UUID:-unknown}")
 
 if [[ -n $ACTIONS_RUNNER_EXTRA_TAGS ]]; then
     runner_tags+=("${ACTIONS_RUNNER_EXTRA_TAGS}")
